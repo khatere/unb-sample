1869384f8e3f548aecbdfe4ca7a46a2e50076148
Khatere <Khatere@Khatere-PC.ad.unb.ca>
2014-07-14 14:25:21 -0300

    some info on for loop

---
+++ lists.py
@@ -0,0 +1,15 @@
+#print range(10)
+#for x in range(3):
+ #   print "x=", x
+name = [['dave', 23 ,'false'],['jeff',25, 'true']]
+for x in name:
+    None
+ #   print x 
+#print name[1][1]
+y = list()
+for x in range(3):
+    y.append(x**2)
+print y
+#x = range(4)          
+#y = [i**2 for i in x]
+#print y       
